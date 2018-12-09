5d9d3d0ea897aea529951c19076677249cc29c1a
Robert A Alfieri <balfieri@nvidia.com>
2018-12-09 02:57:45 -0500

    tweak

--- mpint.h
+++ mpint.h
@@ -26,7 +26,7 @@
 // Typical usage:
 //
 //     #include mpint.h
-//     mpint::implicit_int_w = 128;             // change default int_w to 128 bits
+//     mpint::implicit_int_w_set( 128 );        // change default int_w to 128 bits (from 64)
 //     mpint i;                                 // will get allocated 128 bits and initialized to 0
 //     mpint j( 12 );                           // will get allocated 128 bits and initialized to 12
 //     mpint k( 10, 56 );                       // will get allocated 56  bits and initialized to 10
